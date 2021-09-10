02c89603acfda7267dca1a24db00e9919f68270e
Lodis <lodis.perkins@aie.edu>
2021-09-10 10:14:50 -0500

    Completed array exercises

--- FightSimulation/Game.cs
+++ FightSimulation/Game.cs
@@ -24,15 +24,29 @@ namespace FightSimulation
         Monster backupWompus;
         Monster unclePhil;
 
-        int currentMonsterIndex = 1;
+        int currentMonsterIndex = 0;
+        int currentScene = 0;
 
         public void Run()
         {
+
+            int[] arr = { 1,2,3,4,5 };
+            PrintSmallestAndLargest(arr);
             Start();
 
             while (!gameOver)
             {
                 Update();
+            }
+
+            End();
+        }
+
+        void PrintArray(int[] numArray, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(numArray[i]);
             }
         }
 
@@ -62,19 +76,281 @@ namespace FightSimulation
             unclePhil.defense = 0;
             unclePhil.health = 1.0f;
 
+            ResetCurrentMonsters();
+        }
+
+        /// <summary>
+        /// Called every game loop
+        /// </summary>
+        void Update()
+        {
+            UpdateCurrentScene();
+            Console.Clear();
+        }
+
+        void End()
+        {
+            Console.WriteLine("Guhbah fren");
+        }
+
+        void DisplayUnclePhil()
+        {
+            Console.Write("\n" +
+"                               .ck0KKXXXXNNNNNNNNXNNNXXXXXXXNNNNNNNNNNNNNNNXkc.                                         " +
+"                             .ckO0KKKXXXXXNNNNNNNXXXXXXKKKKKKKXXXXNNNNNNNNNNNX0o,                                       " +
+"                           .ckOO00KKKKXKKKKKKXXXXXKKKKK0000000KXXXXXXXNNNNNNNNNXKd'                                     " +
+"                         .;xO0000KKK0KK000K00KKKK00000OOOOkkOOO0KKK00KXXXXNNNNXXXKk;                                    " +
+"                        .lxO000000000000000000000OOOOkkOOOkkkkkOO000OO0KKKXXXXXXXK0k:.                                  " +
+"                       'coxkOOOOOOOOOOOkkkOOOOOOOOOOkkkkkkkkkkkkkOOOOOOO00KKKKKKXXK0Ol.                                 " +
+"                      '::coxkkkkkkkxxxxxxkkkkkkkkkkkxxxxxxkkkkkkkkkOOOOOOO000000KKK00Oo.                                " +
+"                     ';;:cldxkkkxxxxxxxxxxxkkxxxxxxxxxxxxxxkkkkkkkkkkkkOOOOOOOO000000OOo.                               " +
+"                    .,;;;cldxxxxxxxkkxxxxxxxxxxxxddxxxxxxkkxxkkkkkkkkkkkkkkkkkkOOOOOOOOOo.                              " +
+"                    ';;;:clodddddxxxxkkxxddxxxxxddddxxxxxkkkkkkkkkkkkkkkkkkkkkkkOOOOOOOkko.                             " +
+"                   .,,;;;:clodddddxxxxxxxdddddddddddxxxxxkkkkkkkkkkkkkkkkkxxkkkkkOkkkkOOOkl.                            " +
+"                   .,,;;:::clooooddddxxxdddddddddddddxxxxxxxxxkkkkkkkkkkkkkkkkkkOOOkkOOOOkk:                            " +
+"                   .,;;;;::cllloooooddddddddddddddddxxxxxxxxxxkkkkkkkxkkxxkkkkkkOOOOOOOOkkOd'                           " +
+"                   .,;;;;;;:clllllooodddddddddddxddxxxxxxxxkkkkkkkkkxxxxxxxkkkkkkOO00000OOOk:                           " +
+"                   .,,;;;;;;:cccllllooddddddddddxxxxxxxkkkkkkkkkkkkxxxxxxxxkkkkkOOOO000000OOl.                          " +
+"                   .,,,,;;;;;::ccllllodddddddxxxxxxxxdxxxxxkkkkkkkxxxxxxxxxxxxxxxddoodddxkOOd.                          " +
+"                   .,,,,,;,;;;::cllloooddddxxxxxdooooollllllllloddddddddddddddollc:::ccllodxx'                          " +
+"                   .,,,,,,,;;::cllooooooodddddoollccllccc::;;::cccllllodddoollcc::::cccccllod:                          " +
+"                   .,,,,,,,;;:cllooooooooollllccccclllllcc:;;;:::::cclooddolccc::::;;;;;;;:coo.                         " +
+"                   .,,,,,;;;;:lolllllooollc:::::::;;::;;;;;,;;;::::cclooddollccclddlccc:;,;cod,                         " +
+"                 .;coc;,;;;;;:llllllllllc::::;;;,;:odoccc::;;;::::cclloddddoollodxoc:::cccclox:                         " +
+"                 ;ccllc:;;;;;:cllcccllllc::;;;;;;:cllc:::ccc:cclllllooddxddddoollllllooodxxxkOo.                        " +
+"                .;;;;;:cc:;;;ccccccllllllllllllcllllllllllllloollloooodxxxxdddddolllllclodkO0Ok:                        " +
+"                ';;;::ccc::;;:ccccclllooooddddoolllllllllooodddooooooddxxxxxdddxxddoooodkO000OOk,                       " +
+"                .:::cllc::::::::cclloddddddddxxxxxxxxxxxxxxxxxxdddddddxxkO00kxxxxxxxxxkkO00KK0OOd.                      " +
+"                .;:cllc:;;:c:::cccloodddddxxxkkkkkkkkkkkkkkkkxdddxxxxxkkkO0KKkxdddxxkkkkkkOO000Ok;                      " +
+"                .;::ccc:;;cc:::cccllooddddxxxxxkkkkkOkkkkxddoooddxxxxxkkkOO00OO00kddxxxxkkkkOOOkk:                      " +
+"                 ,::cc::::cc:::::cccloooodxxxxxxkkkOkkxxddolllodxxxxxxxxxkOOkkO0K0kdodddxxkkkxxxx:                      " +
+"                 .;:ccc:::cc;;;;:::cclllloooddddxxxxxxddollcccclllllllloooddxddooooolllooddddddddc.                     " +
+"                  '::ccc::::;;;;;;::ccclcllloooddddddddollccc::;;;;;,;;:cccllc:;::clcccccllooodddc.                     " +
+"                  .;::ccc::;;:;;;;;:::cccccllloooooooolllllcccc::::::;;;;;;:::;;::cccc::::cllloloc.                     " +
+"                   '::cccc::::;;;;;;::::::ccclllollllccccllcc::::::;;;;;;;::;;,;;;;;;:;;;;;::cccl:.                     " +
+"                    ':ccc::::::;;;;;;;::::::cccllcc::;;;;;;;;;;,,;,;;,,;cccc:;,,,,,,,,,,,,,;;:::c:.                     " +
+"                     ,llc:::::::::::;;;;;;::::ccc:;;,,,,,,,,,,;;;;;::::lodooolc:::;;,,,,,,,,;;;;::.                     " +
+"                     .c:;;;;;::::::;;;;;;;;:::::;,,,,,''',,,,,,;;;;;;;:::ccccc:::;;;,,,,,,,,;;;;:;.                     " +
+"                      ..,;;;;;:::::;;::;;;;;;;::;,,,,,,,,,,,,,,;::colodxddkOkkkxl;;;;;;,,,,,;;;:c:.                     " +
+"                        .:;;;;;;;;;;;::;;;;;;;;::,,,,,,;;;;;;::cllodooxkxxkkxxddlccccc:;,,,,,;;:l;                      " +
+"                        .;;;;;;,,,,,;;::;;;;;;;;;;,,,,,;::::::ccclllooddxdxdddoolllllc:;,,,,,;;c:.                      " +
+"                        .;;;;;;,,,;;,;::;;;;,,,,,,,,,,,;::::::clllllooddxxxxddoooolllc;;;,,,,;:c,                       " +
+"                        .:;;;;;;;;;;;;;;;;;;,,,,,,,,,,,;:ccc::ccccccllllllllllllllllc:;,,,,,;:cl'                       " +
+"                      ..';;;;;;;;;;;;;;;;;;;;,,,,,,,,,,,;:::ccccc:::::;;;;;;;;;:cclc:;;,,,,;:col.                       " +
+"                    .'';lo;;;;;;;;;;;;;;;;;;;;;,,,,,,,,,,;::ccccccccc:::::::;;:cclcc:;;;,,;:codc.                       " +
+"                   .,,',dOo:;;;::::;;;;;;,;;,,;,,,,,;;,;,,;;::clllloolllllllccllllc::::;;;::lod;                        " +
+"                  .,,,',oOOkoc::::::;;,,,,,,,,,,,,,;;;;;;,,;;;::ccllccloooooolllllcccc:::::clo:.                        " +
+"                .',,,,,;dkkOOkdlc::::;;;;;;,,,;;,,,;;,,;;;;;;:::::cc::cllclolccccccllllc::col,                          " +
+"               .',,,,,,,lxkkkkkkxlcc::;:;;;:;;;;;;;;;;;;::;::::::::::::::::cc::::cclooollool'                           " +
+"              .',,,,,,,,:dOOOkxxkkxdl::::::cc:::::::::;;::;;:::;;;;;;;;;;,;;;;;;:clloddddoc;,.                          " +
+"             .,,,,,,,,,,;:oO00Okxxxxxol:::ccc::::::c::;;;;;;;;;;;;;,,,,,,,,,,,;:cclllldxdc;,,;,.                        " +
+"            .,,,,,,,,,,,,,;cxKK0Okkxxxdoolccc:ccccc:::::;;;;;;;;;,,,,,,,,,,,,;;:ccclooxOx;,,,,,;,'.........             " +
+"        ...';;,,,,;;,,,,,,,,:d0XXK0Okxddxdolcc:::cccc:::;;;;;;;,,,;,,,,,',,,,;;;:clok0KN0c,,,,,,;:cclllllclcc:;;,'..... " +
+"    ..',::;;;;;;;;;;;,,,,,,,,;ckKXKKKK0Okxxddolc::ccccc:::;;;;;,,,,,,,,,,,,,,,,:ldxOKXNNNOlc;,,,,,;:cllooooooooooollllcc" +
+".,;::::cc:;;,;;;;;;;;,,,,,,,,,,;oOXXKXXXK0OOkkkxdolccc::c::::::;;;;;,,,,,,,;;;coxO0XNNNNNXkdo:,,,,,;:cloooooooooooooolll" +
+"ccclllcc:;;,,,;;;;;,,,,,,,,,,,,,,:dOXXXXXXXKK0OOkxxddolcc::::::::::;;,,;;;:codk0KKXNNNNNNNKkkdc;,,,;:cclloooddooollooooo" +
+"lllollc:;;;,,,,,;,,,,,,,,,,,,,,,,,;cxKXXXNNNNNXKK0OkkOOkxdoc:;;;;;;,,,,,;;;:lOKKKXNNNNNNNNXK0Oxl:,,,;;:ccllooooooolloooo" +
+"lllcc::;;;,,,,,,,,,,,,,,,,,,,,,,,,,,;oOXXNNNNNNNNXXKK00000kl;;,,,,'''''''',,,ck0XNNNNNNNNNNNXKOdl:;;,,;::ccllooooollllll" +
+"ccc::;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,;cxKXNNNNNNNNNNNXXKKKkoclodc;,''',,,,,;:;ckXNNNNNNNNNNNNXKKKOxc;,;;::cccllooollllll" +
+"cc:;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,:d0XNNNNNNNNNNNNNNK000OO0koc:,,,,,,;lO0xdOXNNNNNNNNNNNNNNNN0o;;;;:::cccllollllloo" +
+":::;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;cxKXNNNNNNNNNNNNK0XXXXXK00Od:,,,,,;oKXOdxKNNNNNNNNNNNNNNNXx:;;;;;:::cclllllllll" +
+"::;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;lkKXXNNNNNNNNNK0KKKKXXKKKOo;,,,,,,cxOxllkXNNNNNNNNNNNNNNKo;;;;;;:::cccllllccc" +
+":;;;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;ck0KKNNNNNNNNXKXXXXXKK00ko:;,,,,,,cxkl:cxKNNNNNNNNNNNNNXd:;;;;;:::::clllllll\n");
+        }
+
+        void ResetCurrentMonsters()
+        {
+            currentMonsterIndex = 0;
             //Set starting fighters
             currentMonster1 = GetMonster(currentMonsterIndex);
             currentMonsterIndex++;
             currentMonster2 = GetMonster(currentMonsterIndex);
         }
 
-        void Update()
+        void UpdateCurrentScene()
         {
-            Battle();
-            UpdateCurrentMonsters();
-            Console.ReadKey(true);
-            Console.Clear();
+            switch (currentScene)
+            {
+                case 0:
+                    DisplayStartMenu();
+                    break;
+
+                case 1:
+                    Battle();
+                    UpdateCurrentMonsters();
+                    Console.ReadKey();
+                    break;
+
+                case 2:
+                    DisplayRestartMenu();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid scene index");
+                    break;
+            }
         }
+
+        /// <summary>
+        /// Gets an input from the player based on some decision
+        /// </summary>
+        /// <param name="description">The context for the decision</param>
+        /// <param name="option1">The first choice the player has</param>
+        /// <param name="option2">The second choice the player has</param>
+        /// <param name="pauseInvalid">If true, the player must press a key to continue after inputting
+        /// an incorrect value</param>
+        /// <returns>A number representing which of the two options was choosen. Returns 0 if an invalid input
+        /// was recieved</returns>
+        int GetInput(string description, string option1, string option2, bool pauseInvalid = false)
+        {
+            //Print the context and options
+            Console.WriteLine(description);
+            Console.WriteLine("1. " + option1);
+            Console.WriteLine("2. " + option2);
+
+            //Get player input
+            string input = Console.ReadLine();
+            int choice = 0;
+
+            //If the player typed 1...
+            if (input == "1")
+            {
+                //set the return variable to be 1
+                choice = 1;
+            }
+            //If the player typed 2...
+            else if(input == "2")
+            {
+                //...set the return variable to be 2
+                choice = 2;
+            }
+            //If the player did not type a 1 or 2...
+            else
+            {
+                //...let them know the input was invalid
+                Console.WriteLine("Invalid Input");
+
+                //If we want to pause when an invalid input is recieved...
+                if (pauseInvalid)
+                {
+                    //...make the player press a key to continue
+                    Console.ReadKey(true);
+                }
+            }
+
+            //Return the player choice
+            return choice;
+        }
+
+        void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        void PrintSmallestAndLargest(int[] arr)
+        {
+            int largest = arr[0];
+            int smallest = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+
+                if (arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                }
+            }
+
+            Console.WriteLine("Largest: " + largest);
+            Console.WriteLine("Smallest: " + smallest);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        void PrintSmallestAndLargestFinished(int[] arr)
+        {
+            int largest = arr[0];
+            int smallest = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > largest)
+                    largest = arr[i];
+                else if (arr[i] < smallest)
+                    smallest = arr[i];
+            }
+
+            Console.WriteLine("Largest: " + largest);
+            Console.WriteLine("Smallest: " + smallest);
+        }
+
+        /// <summary>
+        /// Displays the starting menu. Gives the player the option to start or
+        /// exit the simulation.
+        /// </summary>
+        void DisplayStartMenu()
+        {
+            //Get player choice
+            int choice = GetInput("Welcome to Monster Fight Simulator And Uncle Phil", "Start Simulation", "Quit Application");
+            DisplayUnclePhil();
+            Console.ReadKey(true);
+            //If they chose to start the simulation...
+            if (choice == 1)
+            {
+                //...start the battle scene
+                currentScene = 1;
+            }
+            //Otherwise if they chose to exit...
+            else if (choice == 2)
+            {
+                //...end the game
+                gameOver = true;
+            }
+        }
+
+        /// <summary>
+        /// Displays the restart menu. Gives the player the option to restart or exit the program
+        /// </summary>
+        void DisplayRestartMenu()
+        {
+            //Get the player choice
+            int choice = GetInput("Simulation over. Would you like to play again?", "Yes", "No");
+
+            //If the player chose to restart...
+            if (choice == 1)
+            {
+                //...set the current scene to be the start menu
+                ResetCurrentMonsters();
+                currentScene = 0;
+            }
+            //If the player chose to exit..
+            else if (choice == 2)
+            {
+                //...end the game
+                gameOver = true;
+            }
+        }
+
+        
 
         Monster GetMonster(int monsterIndex)
         {
@@ -84,19 +360,19 @@ namespace FightSimulation
             monster.defense = 1;
             monster.health = 1;
 
-            if (monsterIndex == 1)
+            if (monsterIndex == 0)
             {
                 monster = unclePhil;
             }
-            else if (monsterIndex == 2)
+            else if (monsterIndex == 1)
             {
                 monster = backupWompus;
             }
-            else if (monsterIndex == 3)
+            else if (monsterIndex == 2)
             {
                 monster = wompus;
             }
-            else if (monsterIndex == 4)
+            else if (monsterIndex == 3)
             {
                 monster = thwompus;
             }
@@ -146,9 +422,8 @@ namespace FightSimulation
             //If either monster is set to "None" and the last monster has been set...
             if (currentMonster2.name == "None" || currentMonster1.name == "None" && currentMonsterIndex >= 4)
             {
-                //...end the game
-                Console.WriteLine("Simulation Over");
-                gameOver = true;
+                //...go to the restart menu
+                currentScene = 2;
             }
         }
 
